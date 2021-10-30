265d272164c6e9c2ed2ea662447819c80c8b3c0b
Katrina Yeo <katrina.yeo@tsgforce.com>
2021-10-30 12:20:15 +0800

    Unemployment NNAR

--- unemploymentPrediction.R
+++ unemploymentPrediction.R
@@ -14,187 +14,184 @@ library(pander)
 library(QuantTools)
 library(lubridate)
 
-#Loading Daily New Cases Data
-DNC <- read_excel("formated_data.xlsx", sheet = "Daily New Cases")
-SGDNC<-DNC$Singapore
-UKDNC<-DNC$`United Kingdom`
-USDNC<-DNC$`United States`
+set.seed(1)
 
 #Loading Monthly Unemployment Rate Data
-MUR <- read_excel("formated_data.xlsx", sheet = "Monthly Unemployment Rate")
+MUR <- read_excel("formated_data.xlsx", sheet = "Extended Unemployment Rate")
 SGMUR<-MUR$Singapore
 UKMUR<-MUR$`United Kingdom`
 USMUR<-MUR$`United States`
 
-#Loading Government Response Index Data
-GRI <- read_excel("formated_data.xlsx", sheet = "Government Response Index")
-SGGRI<-GRI$Singapore
-UKGRI<-GRI$`United Kingdom`
-USGRI<-GRI$`United States`
-
-#Loading Containment Health Index Data
-CHI <- read_excel("formated_data.xlsx", sheet = "Containment Health Index")
-SGCHI<-CHI$Singapore
-UKCHI<-CHI$`United Kingdom`
-USCHI<-CHI$`United States`
-
-#Loading Economic Support Index Data
-ESI <- read_excel("formated_data.xlsx", sheet = "Economic Support Index")
-SGESI<-ESI$Singapore
-UKESI<-ESI$`United Kingdom`
-USESI<-ESI$`United States`
-
 ####NNAR model for Singapore####
-plot(SGMUR)
-hist(SGMUR)
-summary(SGMUR)
-data.frame(kurtosis=kurtosis(SGMUR))   # calculate Coefficient of kurtosis
-data.frame(skewness=skewness(SGMUR))  # calculate Coefficient of skewness
-data.frame(Standard.deviation=sd(SGMUR))
-
-# Data Preprocessing
+original_data=SGMUR[33:53]
 y_lab <- "Monthly Unemployment rate in SG"   # input name of data
-NNAR_Model<- TRUE     #create new model (TRUE/FALSE)
-Number_Neural<-5  # Number of Neural For model NNAR Model
-#processing on data (input data)
-rows <- NROW(SGMUR) # calculate number of rows in time series (number of months)
-validation_data_months = 3
-training_data<-SGMUR[1:(rows-validation_data_months)] # Training data
-testing_data<-SGMUR[(rows-validation_data_months+1):rows] #Testing data
+rows <- NROW(original_data) # calculate number of rows in time series (number of days)
+training_data<-original_data[1:17] # Training data
+testing_data_start_index = 18
+testing_data<-original_data[testing_data_start_index:21] #testing data
 
-if(NNAR_Model==TRUE){
-  data_series<-ts(training_data)
-  model_NNAR<-nnetar(data_series, size = Number_Neural)
-  saveRDS(model_NNAR, file = "model_NNAR.RDS")
-  my_model <- readRDS("model_NNAR.RDS")
-  model_NNAR
-}
-
+AD<-MUR$Date # Input range for actual date
+Forecast_date_interval <- c("2021/10/01","2021/10/31")
 frequency<-"months"
-Actual_date_interval <- c("2020/01/01","2021/08/31")
-Forecast_date_interval <- c("2021/09/01","2021/10/31")
+FD<-seq(as.Date(Forecast_date_interval[1]), as.Date(Forecast_date_interval[2]), frequency)  # Input range forecasting date
+N_forecasting_months<-nrow(data.frame(FD))  # Number of days to forecast
+validation_dates<-tail(AD, NROW(testing_data)) # Number of validation dates
+no_validation_data_months<-NROW(testing_data)
 
-AD<-fulldate<-seq(as.Date(Actual_date_interval[1]),as.Date(Actual_date_interval[2]), frequency)  #input range for actual date
-FD<-seq(as.Date(Forecast_date_interval[1]),as.Date(Forecast_date_interval[2]), frequency)  #input range forecasting date
 
-N_forecasting_months<-nrow(data.frame(FD))  #calculate number of months that you want to forecast
-validation_dates<-tail(AD,validation_data_months) # select validation_dates
+data_series<-ts(training_data)
+#Number_Neural<-1    # Number of Neural For model NNAR Model
+model_NNAR<-nnetar(data_series)
+accuracy(model_NNAR)  # Accuracy on training data #Print Model Parameters
+model_NNAR
 
-forecasting_NNAR <- forecast(model_NNAR, h=N_forecasting_months+validation_data_months)
-validation_forecast<-head(forecasting_NNAR$mean,validation_data_months)
-MSE_Per_Month<-round((testing_data-validation_forecast)^2, 3)
-
-MSE_Mean_All<-paste(round(mean(MSE_Per_Month),3),"% average MSE for",validation_data_months,frequency,y_lab,sep=" ")
-print(MSE_Mean_All)
-
-MSE_NNAR_Model<-paste(MSE_Per_Month ,"%")
-print(ascii(data.frame(date_NNAR=validation_dates,actual_data=testing_data,forecasting_NNAR=validation_forecast,MSE_NNAR_Model)), type = "rest")
-print(ascii(data.frame(FD,forecasting_by_NNAR=tail(forecasting_NNAR$mean,N_forecasting_months))), type = "rest")
-plot(forecasting_NNAR,xlab = paste ("Time in", frequency ,y_lab , sep=" "), ylab=y_lab)
-x1_test <- ts(testing_data, start =(rows-validation_data_months+1) )
+# Testing Data Evaluation
+one_month_ahead_forecast <- forecast(model_NNAR, h=1)
+se_1<-(testing_data[1]-one_month_ahead_forecast$mean)^2
+se_vec<-c(se_1)
+forecast_vec = c(one_month_ahead_forecast$mean)
+for (i in 1:(NROW(testing_data)-1)){
+  input_data<-c(training_data, testing_data[1:i])
+  temp_model<-nnetar(input_data, model=model_NNAR)
+  actual_data<-testing_data[i+1]
+  one_month_ahead_forecast <- forecast(temp_model, h=1)
+  forecast_vec<-c(forecast_vec, one_month_ahead_forecast$mean)
+  se<-(actual_data-one_month_ahead_forecast$mean)^2
+  se_vec<-c(se_vec, se)
+}
+plot(c(training_data, forecast_vec),xlab = paste ("Time in", frequency ,y_lab , sep=" "), ylab=y_lab, type='l')
+x1_test <- ts(testing_data, start=NROW(training_data)+1 )
 lines(x1_test, col='red',lwd=2)
+
+MSE_Per_Day<-round(mean(se_vec), 3)
+paste ("MSE % For",no_validation_data_months,frequency,"by using NNAR Model for  ==> ",y_lab, sep=" ")
+MSE_Mean_All<-paste(MSE_Per_Day," MSE ",no_validation_data_months,frequency,y_lab,sep=" ")
+paste ("MSE that's Error of Forecasting for ",no_validation_data_months," months in NNAR Model for  ==> ",y_lab, sep=" ")
+paste(MSE_Mean_All)
+
+NNAR_forecast_model<-nnetar(original_data, model=model_NNAR)
+NNAR_forecast <- forecast(NNAR_forecast_model, h=N_forecasting_months)
+print(ascii(data.frame(FD, forecasting_by_NNAR=NNAR_forecast$mean)), type = "rest")
+plot(c(original_data, NNAR_forecast$mean), xlab = paste ("Time in", frequency ,y_lab , sep=" "), ylab=y_lab, type='l')
+
 
 
 
 ####NNAR model for UK####
-plot(UKMUR)
-hist(UKMUR)
-summary(UKMUR)
-data.frame(kurtosis=kurtosis(UKMUR))   # calculate Coefficient of kurtosis
-data.frame(skewness=skewness(UKMUR))  # calculate Coefficient of skewness
-data.frame(Standard.deviation=sd(UKMUR))
-
-# Data Preprocessing
+original_data=UKMUR
 y_lab <- "Monthly Unemployment rate in UK"   # input name of data
-NNAR_Model<- TRUE     #create new model (TRUE/FALSE)
-Number_Neural<-5  # Number of Neural For model NNAR Model
-#processing on data (input data)
-rows <- NROW(UKMUR) # calculate number of rows in time series (number of months)
-validation_data_months = 3
-training_data<-UKMUR[1:(rows-validation_data_months)] # Training data
-testing_data<-UKMUR[(rows-validation_data_months+1):rows] #Testing data
+rows <- NROW(original_data) # calculate number of rows in time series (number of days)
+training_data<-original_data[1:49] # Training data
+testing_data_start_index = 50
+testing_data<-original_data[testing_data_start_index:53] #testing data
 
-if(NNAR_Model==TRUE){
-  data_series<-ts(training_data)
-  model_NNAR<-nnetar(data_series, size = Number_Neural)
-  saveRDS(model_NNAR, file = "model_NNAR.RDS")
-  my_model <- readRDS("model_NNAR.RDS")
-  model_NNAR
-}
-
+AD<-MUR$Date # Input range for actual date
+Forecast_date_interval <- c("2021/10/01","2021/10/31")
 frequency<-"months"
-Actual_date_interval <- c("2020/01/01","2021/08/31")
-Forecast_date_interval <- c("2021/09/01","2021/10/31")
+FD<-seq(as.Date(Forecast_date_interval[1]), as.Date(Forecast_date_interval[2]), frequency)  # Input range forecasting date
+N_forecasting_months<-nrow(data.frame(FD))  # Number of days to forecast
+validation_dates<-tail(AD, NROW(testing_data)) # Number of validation dates
+no_validation_data_months<-NROW(testing_data)
 
-AD<-fulldate<-seq(as.Date(Actual_date_interval[1]),as.Date(Actual_date_interval[2]), frequency)  #input range for actual date
-FD<-seq(as.Date(Forecast_date_interval[1]),as.Date(Forecast_date_interval[2]), frequency)  #input range forecasting date
 
-N_forecasting_months<-nrow(data.frame(FD))  #calculate number of months that you want to forecast
-validation_dates<-tail(AD,validation_data_months) # select validation_dates
+data_series<-ts(training_data)
+#Number_Neural<-1    # Number of Neural For model NNAR Model
+model_NNAR<-nnetar(data_series)
+accuracy(model_NNAR)  # Accuracy on training data #Print Model Parameters
+model_NNAR
 
-forecasting_NNAR <- forecast(model_NNAR, h=N_forecasting_months+validation_data_months)
-validation_forecast<-head(forecasting_NNAR$mean,validation_data_months)
-MSE_Per_Month<-round((testing_data-validation_forecast)^2, 3)
-
-MSE_Mean_All<-paste(round(mean(MSE_Per_Month),3),"% average MSE for",validation_data_months,frequency,y_lab,sep=" ")
-print(MSE_Mean_All)
-
-MSE_NNAR_Model<-paste(MSE_Per_Month ,"%")
-print(ascii(data.frame(date_NNAR=validation_dates,actual_data=testing_data,forecasting_NNAR=validation_forecast,MSE_NNAR_Model)), type = "rest")
-print(ascii(data.frame(FD,forecasting_by_NNAR=tail(forecasting_NNAR$mean,N_forecasting_months))), type = "rest")
-plot(forecasting_NNAR,xlab = paste ("Time in", frequency ,y_lab , sep=" "), ylab=y_lab)
-x1_test <- ts(testing_data, start =(rows-validation_data_months+1) )
+# Testing Data Evaluation
+one_month_ahead_forecast <- forecast(model_NNAR, h=1)
+one_month_ahead_forecast["fitted"]
+one_month_ahead_forecast["residuals"]
+se_1<-(testing_data[1]-one_month_ahead_forecast$mean)^2
+se_vec<-c(se_1)
+forecast_vec = c(one_month_ahead_forecast$mean)
+for (i in 1:(NROW(testing_data)-1)){
+  input_data<-c(training_data, testing_data[1:i])
+  temp_model<-nnetar(input_data, model=model_NNAR)
+  actual_data<-testing_data[i+1]
+  one_month_ahead_forecast <- forecast(temp_model, h=1)
+  forecast_vec<-c(forecast_vec, one_month_ahead_forecast$mean)
+  se<-(actual_data-one_month_ahead_forecast$mean)^2
+  se_vec<-c(se_vec, se)
+}
+plot(c(training_data, forecast_vec),xlab = paste ("Time in", frequency ,y_lab , sep=" "), ylab=y_lab, type='l')
+x1_test <- ts(testing_data, start=NROW(training_data)+1 )
 lines(x1_test, col='red',lwd=2)
+
+MSE_Per_Day<-round(mean(se_vec), 3)
+paste ("MSE % For",no_validation_data_months,frequency,"by using NNAR Model for  ==> ",y_lab, sep=" ")
+MSE_Mean_All<-paste(MSE_Per_Day," MSE ",no_validation_data_months,frequency,y_lab,sep=" ")
+paste ("MSE that's Error of Forecasting for ",no_validation_data_months," months in NNAR Model for  ==> ",y_lab, sep=" ")
+paste(MSE_Mean_All)
+
+NNAR_forecast_model<-nnetar(original_data, model=model_NNAR)
+NNAR_forecast <- forecast(NNAR_forecast_model, h=N_forecasting_months)
+print(ascii(data.frame(FD, forecasting_by_NNAR=NNAR_forecast$mean)), type = "rest")
+plot(c(original_data, NNAR_forecast$mean), xlab = paste ("Time in", frequency ,y_lab , sep=" "), ylab=y_lab, type='l')
+
+
+
+
+
+
 
 ####NNAR model for US####
-plot(USMUR)
-hist(USMUR)
-summary(USMUR)
-data.frame(kurtosis=kurtosis(USMUR))   # calculate Coefficient of kurtosis
-data.frame(skewness=skewness(USMUR))  # calculate Coefficient of skewness
-data.frame(Standard.deviation=sd(USMUR))
+original_data=USMUR
+y_lab <- "Monthly Unemployment rate in US"   # input name of data
+rows <- NROW(original_data) # calculate number of rows in time series (number of days)
+training_data<-original_data[1:49] # Training data
+testing_data_start_index = 50
+testing_data<-original_data[testing_data_start_index:53] #testing data
 
-# Data Preprocessing
-y_lab <- "Monthly Unemployment rate in UK"   # input name of data
-NNAR_Model<- TRUE     #create new model (TRUE/FALSE)
-Number_Neural<-5  # Number of Neural For model NNAR Model
-#processing on data (input data)
-rows <- NROW(USMUR) # calculate number of rows in time series (number of months)
-validation_data_months = 3
-training_data<-USMUR[1:(rows-validation_data_months)] # Training data
-testing_data<-USMUR[(rows-validation_data_months+1):rows] #Testing data
-
-if(NNAR_Model==TRUE){
-  data_series<-ts(training_data)
-  model_NNAR<-nnetar(data_series, size = Number_Neural)
-  saveRDS(model_NNAR, file = "model_NNAR.RDS")
-  my_model <- readRDS("model_NNAR.RDS")
-  model_NNAR
-}
-
+AD<-MUR$Date # Input range for actual date
+Forecast_date_interval <- c("2021/10/01","2021/10/31")
 frequency<-"months"
-Actual_date_interval <- c("2020/01/01","2021/08/31")
-Forecast_date_interval <- c("2021/09/01","2021/10/31")
+FD<-seq(as.Date(Forecast_date_interval[1]), as.Date(Forecast_date_interval[2]), frequency)  # Input range forecasting date
+N_forecasting_months<-nrow(data.frame(FD))  # Number of days to forecast
+validation_dates<-tail(AD, NROW(testing_data)) # Number of validation dates
+no_validation_data_months<-NROW(testing_data)
 
-AD<-fulldate<-seq(as.Date(Actual_date_interval[1]),as.Date(Actual_date_interval[2]), frequency)  #input range for actual date
-FD<-seq(as.Date(Forecast_date_interval[1]),as.Date(Forecast_date_interval[2]), frequency)  #input range forecasting date
 
-N_forecasting_months<-nrow(data.frame(FD))  #calculate number of months that you want to forecast
-validation_dates<-tail(AD,validation_data_months) # select validation_dates
+data_series<-ts(training_data)
+#Number_Neural<-1    # Number of Neural For model NNAR Model
+model_NNAR<-nnetar(data_series)
+accuracy(model_NNAR)  # Accuracy on training data #Print Model Parameters
+model_NNAR
 
-forecasting_NNAR <- forecast(model_NNAR, h=N_forecasting_months+validation_data_months)
-validation_forecast<-head(forecasting_NNAR$mean,validation_data_months)
-MSE_Per_Month<-round((testing_data-validation_forecast)^2, 3)
-
-MSE_Mean_All<-paste(round(mean(MSE_Per_Month),3),"% average MSE for",validation_data_months,frequency,y_lab,sep=" ")
-print(MSE_Mean_All)
-
-MSE_NNAR_Model<-paste(MSE_Per_Month ,"%")
-print(ascii(data.frame(date_NNAR=validation_dates,actual_data=testing_data,forecasting_NNAR=validation_forecast,MSE_NNAR_Model)), type = "rest")
-print(ascii(data.frame(FD,forecasting_by_NNAR=tail(forecasting_NNAR$mean,N_forecasting_months))), type = "rest")
-plot(forecasting_NNAR,xlab = paste ("Time in", frequency ,y_lab , sep=" "), ylab=y_lab)
-x1_test <- ts(testing_data, start =(rows-validation_data_months+1) )
+# Testing Data Evaluation
+one_month_ahead_forecast <- forecast(model_NNAR, h=1)
+one_month_ahead_forecast["fitted"]
+one_month_ahead_forecast["residuals"]
+se_1<-(testing_data[1]-one_month_ahead_forecast$mean)^2
+se_vec<-c(se_1)
+forecast_vec = c(one_month_ahead_forecast$mean)
+for (i in 1:(NROW(testing_data)-1)){
+  input_data<-c(training_data, testing_data[1:i])
+  temp_model<-nnetar(input_data, model=model_NNAR)
+  actual_data<-testing_data[i+1]
+  one_month_ahead_forecast <- forecast(temp_model, h=1)
+  forecast_vec<-c(forecast_vec, one_month_ahead_forecast$mean)
+  se<-(actual_data-one_month_ahead_forecast$mean)^2
+  se_vec<-c(se_vec, se)
+}
+plot(c(training_data, forecast_vec),xlab = paste ("Time in", frequency ,y_lab , sep=" "), ylab=y_lab, type='l')
+x1_test <- ts(testing_data, start=NROW(training_data)+1 )
 lines(x1_test, col='red',lwd=2)
+
+MSE_Per_Day<-round(mean(se_vec), 3)
+paste ("MSE % For",no_validation_data_months,frequency,"by using NNAR Model for  ==> ",y_lab, sep=" ")
+MSE_Mean_All<-paste(MSE_Per_Day," MSE ",no_validation_data_months,frequency,y_lab,sep=" ")
+paste ("MSE that's Error of Forecasting for ",no_validation_data_months," months in NNAR Model for  ==> ",y_lab, sep=" ")
+paste(MSE_Mean_All)
+
+NNAR_forecast_model<-nnetar(original_data, model=model_NNAR)
+NNAR_forecast <- forecast(NNAR_forecast_model, h=N_forecasting_months)
+print(ascii(data.frame(FD, forecasting_by_NNAR=NNAR_forecast$mean)), type = "rest")
+plot(c(original_data, NNAR_forecast$mean), xlab = paste ("Time in", frequency ,y_lab , sep=" "), ylab=y_lab, type='l')
+
+
 
 
 
